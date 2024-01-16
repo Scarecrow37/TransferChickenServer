c69113a066db3ae53077375f692201d1eaab7511
Suha Do <weqa123@naver.com>
2024-01-16 12:44:28 +0900

    Complete

--- TransferChickenServer/TransferChickenServer.cpp
+++ TransferChickenServer/TransferChickenServer.cpp
@@ -1,40 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <cstdio>
 #include <iostream>
+#include <WinSock2.h>
 
-#include "Network/TCPServer.h"
-#include "Utility/Exception/Exception.h"
-#include "Utility/FileSystem/FileSystem.h"
+#pragma comment(lib, "ws2_32")
+
 
 int main()
 {
-    FileSystem* FSystem = new FileSystem();
-    if (FSystem->Open("./Resource/chicken.jpg", "rb"))
-    {
-        throw Exception::MakeException("File open fail.");
-    }
+    FILE* ChickenImage = {};
+    const errno_t _ = fopen_s(&ChickenImage, "./Resource/chicken.jpg", "rb");
     char Buffer[100000] = {};
-    if (FSystem->ReadFile(Buffer) == 0)
-    {
-        throw Exception::MakeException("File read fail.");
-    }
+    size_t FileSize = fread_s(Buffer, sizeof(Buffer), sizeof(char), sizeof(Buffer), ChickenImage);
+    fclose(ChickenImage);
     
-    std::cout << (Result ? "True" : "False");
-    /*
-    TcpServer* Server = new TcpServer();
-    Server->Initialize();
-    Server->Bind(8080);
-    Server->Listen();
-    const TcpSocket* Socket = Server->Accept();
+    WSADATA WsaData;
+    WSAStartup(MAKEWORD(2, 2), &WsaData);
 
+    SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKADDR_IN ServerAddress = {};
+    ServerAddress.sin_family = AF_INET;
+    ServerAddress.sin_addr.s_addr = ADDR_ANY;
+    ServerAddress.sin_port = htons(8080);
 
-    FILE* Image = fopen("C:/Work/Resource/chicken.jpg", "rb");
-    char Buffer[100000] = {};
-    int BufferSize = sizeof(Buffer);
-    int ReadSize = fread(Buffer, sizeof(char), BufferSize, Image);
-    Socket->Send(Buffer, ReadSize);
-    std::cout << ReadSize;
-    fclose(Image);
-    */
-    return 0; 
+    bind(ListenSocket, reinterpret_cast<SOCKADDR*>(&ServerAddress), sizeof(ServerAddress));
+    listen(ListenSocket, 1);
+
+    SOCKADDR_IN ClientAddress = {};
+    int ClientAddressLength = sizeof(ClientAddress);
+    SOCKET ClientSocket = accept(ListenSocket, reinterpret_cast<SOCKADDR*>(&ClientAddress), &ClientAddressLength);
+
+    send(ClientSocket, Buffer, static_cast<int>(FileSize), 0);
+    
+    return 0;
 }
--- TransferChickenServer/Utility/Exception/Exception.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <exception>
-#include <string>
-
-class Exception
-{
-public:
-    static std::exception MakeException(const std::string& Message);
-};
